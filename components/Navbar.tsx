0d573d1c20117be5be628d31928f1aad7f2a9aaa
Kristoffer Garling <kristoffer.garling@gmail.com>
2023-01-04 18:26:22 +0100

    navbar fix

--- components/Navbar.tsx
+++ components/Navbar.tsx
@@ -4,6 +4,9 @@ import mobilelogo from "../assets/images/logo_mobile.svg";
 
 const Navbar: React.FC = () => {
   return (
+    <div className="text-white bg-black">
+      <h1 className="w-full text-3xl font-bold">Test</h1>
+    </div>
   );
 };
 
